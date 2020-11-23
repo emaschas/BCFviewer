993fb8311b1c1b1f60fb3135c02f44697b6260c1
Emmanuel <emaschas@hotmail.com>
2020-11-23 18:53:16 +0100

    upload

---
+++ BCF_Read.cs
@@ -0,0 +1,264 @@
+﻿
+// Read BCF comments file
+// Emmanuel Maschas - 10-2020
+// Class
+
+#region "Usings"
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Xml.Linq;
+using System.IO;
+using System.IO.Compression;
+using System.Globalization;
+#endregion
+
+namespace BCF_Read {
+
+  #region "BCF Structures"
+
+  public class Viewpoint {
+    public string GUID             {get;set;}
+    public string Index            {get;set;}
+    public string Bcfv             {get;set;}
+    public string Snapshot         {get;set;}
+    public double CamX             {get;set;}
+    public double CamY             {get;set;}
+    public double CamZ             {get;set;}
+    public double DirX             {get;set;}
+    public double DirY             {get;set;}
+    public double DirZ             {get;set;}
+    public double UpX              {get;set;}
+    public double UpY              {get;set;}
+    public double UpZ              {get;set;}
+    public double Field            {get;set;}
+    public List<string> Components {get;set;}
+    public Bitmap Image            {get;set;}
+  }
+
+  public class Comment {
+    public string Date            {get;set;}
+    public string Author          {get;set;}
+    public string Text            {get;set;}
+    public string ModifiedDate    {get;set;}
+    public string ModifiedAuthor  {get;set;}
+    public string VPGuid          {get;set;}
+    public Viewpoint Viewpoint    {get;set;}
+  }
+
+  public class Topic {
+    public string ZipFile             {get;set;}
+    public string TopicType           {get;set;}
+    public string TopicStatus         {get;set;}
+    public string Title               {get;set;}
+    public string Priority            {get;set;}
+    public string Index               {get;set;}
+    public string CreationDate        {get;set;}
+    public string CreationAuthor      {get;set;}
+    public string ModifiedDate        {get;set;}
+    public string ModifiedAuthor      {get;set;}
+    public string Description         {get;set;}
+    public List<Comment> Comments     {get;set;}
+    public List<Viewpoint> Viewpoints {get;set;}
+  }
+
+  #endregion
+
+  /// <summary> BCF file class </summary>
+  public partial class BCFfile : UserControl {
+
+    /// <summary> List of BCF Topics loaded in Navisworks </summary>
+    public List<Topic> TopicsList = new List<Topic>();
+
+    #region "BCF Utilities"
+
+    /// <summary> Utility to read an xml field </summary>
+    /// <returns> The value of the XML element or "-" if unset </returns>
+    private string ReadXML(XElement obj, string field) {
+      if(obj.Element(field) != null)
+        return obj.Element(field).Value;
+      else
+        return "-";
+    }
+    
+    /// <summary> Utility to read an xml attribute </summary>
+    /// <returns> The attribute value or "-" if unset </returns>
+    private string ReadATT(XElement obj, string attribute) {
+      string res = "-";
+      foreach(XAttribute att in obj.Attributes(attribute)) res = att.Value;
+      return res;
+    }
+    
+    /// <summary> Format an ISO date in readable string </summary>
+    /// <param name="isodate">Date in ISO format. Ex : 2014-10-16T14:35:29+00:00</param>
+    private string formatDate(string isodate) {
+      string res;
+      try { 
+        DateTime dat = DateTime.Parse(isodate);
+        res = dat.ToString("d", CultureInfo.CreateSpecificCulture("fr-FR")); 
+      }
+      catch { res = isodate; }
+      return res;
+    }
+
+    #endregion
+
+    #region "Read Routines"
+
+    /// <summary> Read the Camera settings for the selected viewpoint.bcfv file </summary>
+    /// <param name="bcfzip">ZipArchive in which the bcfv file is located</param>
+    /// <param name="filename">Name of bcfv viewpoint file within the ZIP archive</param>
+    /// <param name="NewVP">Viewpoint in which the camera settings will be stored</param>
+    private void readBCFV(ZipArchive bcfzip, string filename, Viewpoint viewpoint) {
+      ZipArchiveEntry bcfvzip = bcfzip.GetEntry(filename);
+      // Default values
+      viewpoint.CamX = 0.0; viewpoint.CamY = 0.0; viewpoint.CamZ = 0.0;
+      viewpoint.DirX = 1.0; viewpoint.DirY = 0.0; viewpoint.DirZ = 0.0;
+      viewpoint.UpX  = 0.0; viewpoint.UpY  = 0.0; viewpoint.UpZ  = 1.0;
+      viewpoint.Field = 0.0; // Identifies unset camera.
+      viewpoint.Components = new List<String>();
+      if(bcfvzip!=null) {
+        XDocument bcfv = XDocument.Load(bcfvzip.Open()); // Parse bcfv XML document
+        XElement camera = bcfv.Root.Element("PerspectiveCamera");
+        if(camera!=null) {
+          XElement point = camera.Element("CameraViewPoint");
+          viewpoint.CamX = Double.Parse(point.Element("X").Value);
+          viewpoint.CamY = Double.Parse(point.Element("Y").Value);
+          viewpoint.CamZ = Double.Parse(point.Element("Z").Value);
+          point = camera.Element("CameraDirection");
+          viewpoint.DirX = Double.Parse(point.Element("X").Value);
+          viewpoint.DirY = Double.Parse(point.Element("Y").Value);
+          viewpoint.DirZ = Double.Parse(point.Element("Z").Value);
+          point = camera.Element("CameraUpVector");
+          viewpoint.UpX = Double.Parse(point.Element("X").Value);
+          viewpoint.UpY = Double.Parse(point.Element("Y").Value);
+          viewpoint.UpZ = Double.Parse(point.Element("Z").Value);
+          viewpoint.Field = Double.Parse(camera.Element("FieldOfView").Value);
+          XElement cps = bcfv.Root.Element("Components");
+          if(cps != null) {
+            foreach(XElement comp in cps.Elements("Component")) {
+              viewpoint.Components.Add(ReadATT(comp, "IfcGuid"));
+            }
+          }
+        }
+      } else {
+        MessageBox.Show(filename + "  Not found");
+      }
+    }
+    
+    /// <summary> Load or Append a BCF File </summary> 
+    /// <param name="FileName">Full path of the file to be loadded or appended </param>
+    /// <param name="Append">Load if false or Append if true </param>
+    public void ReadBCF(String FileName, Boolean Append) {
+      if(!Append) TopicsList.Clear();
+      using(ZipArchive bcfzip = ZipFile.OpenRead(FileName)) {
+        foreach(ZipArchiveEntry entry in bcfzip.Entries) {
+          if(entry.FullName.EndsWith("markup.bcf", StringComparison.OrdinalIgnoreCase)) {
+            string folder = entry.FullName.Substring(0, entry.FullName.LastIndexOf("/") + 1);
+            XDocument markup = XDocument.Load(entry.Open()); // Parse XML document
+            // Topic
+            XElement topic = markup.Root.Element("Topic");
+            Topic NewTopic = new Topic();
+            NewTopic.ZipFile        = FileName;
+            NewTopic.TopicType      = ReadATT(topic, "TopicType");
+            NewTopic.TopicStatus    = ReadATT(topic, "TopicStatus");
+            NewTopic.Title          = ReadXML(topic, "Title");
+            NewTopic.Priority       = ReadXML(topic, "Priority");
+            NewTopic.Index          = ReadXML(topic, "Index");
+            NewTopic.CreationDate   = ReadXML(topic, "CreationDate");
+            NewTopic.CreationAuthor = ReadXML(topic, "CreationAuthor");
+            NewTopic.ModifiedDate   = ReadXML(topic, "ModifiedDate");
+            NewTopic.ModifiedAuthor = ReadXML(topic, "ModifiedAuthor");
+            NewTopic.Description    = ReadXML(topic, "Description");
+            if(NewTopic.ModifiedDate  =="-") NewTopic.ModifiedDate=NewTopic.CreationDate;
+            if(NewTopic.ModifiedAuthor=="-") NewTopic.ModifiedAuthor=NewTopic.CreationAuthor;
+            // Comments
+            NewTopic.Comments = new List<Comment>();
+            foreach(XElement com in markup.Root.Elements("Comment")) {
+              Comment NewComment = new Comment();
+              NewComment.Text =           ReadXML(com, "Comment");
+              NewComment.Date =           ReadXML(com, "Date");
+              NewComment.Author =         ReadXML(com, "Author");
+              NewComment.ModifiedAuthor = ReadXML(com, "ModifiedAuthor");
+              NewComment.ModifiedDate =   ReadXML(com, "ModifiedDate");
+              if(NewComment.ModifiedAuthor=="-") NewComment.ModifiedAuthor=NewComment.Author;
+              if(NewComment.ModifiedDate  =="-") NewComment.ModifiedDate=NewComment.Date;
+              XElement vp = com.Element("Viewpoint");
+              NewComment.VPGuid = (vp != null ? ReadATT(vp, "Guid") : "-");
+              NewTopic.Comments.Add(NewComment);
+            }
+            // Viewpoints
+            NewTopic.Viewpoints = new List<Viewpoint>();
+            // Search for Viewpoints in Version 2.0
+            foreach(XElement vp in markup.Root.Elements("Viewpoints")) {
+              Viewpoint NewVP = new Viewpoint();
+              NewVP.GUID     = ReadATT(vp, "Guid");
+              NewVP.Bcfv     = ReadXML(vp, "Viewpoint");
+              NewVP.Snapshot = ReadXML(vp, "Snapshot");
+              if(NewVP.Snapshot!="-") NewVP.Snapshot = folder + NewVP.Snapshot;
+              readBCFV(bcfzip, folder + NewVP.Bcfv, NewVP);
+              // Viewpoint-Image
+              try {
+                if(NewVP.Snapshot!="-") {
+                  ZipArchiveEntry snap = bcfzip.GetEntry(NewVP.Snapshot);
+                  Bitmap img = new Bitmap(snap.Open());
+                  NewVP.Image = img;
+                }
+              } catch {
+                MessageBox.Show("Error Image");
+              }
+              NewTopic.Viewpoints.Add(NewVP);
+            }
+            // If Viewpoints is empty : 
+            // try version 1.0 BCF : viewpoint.bcfv and snapshot.png files.
+            if(NewTopic.Viewpoints.Count == 0) {
+              ZipArchiveEntry bcfvzip = bcfzip.GetEntry(folder + "viewpoint.bcfv");
+              if(bcfvzip!=null) { // Found viewpoint.bcfv !
+                Viewpoint NewVP = new Viewpoint();
+                NewVP.GUID = "viewpoint.bcfv"; // pseudo att.
+                NewVP.Bcfv = "viewpoint.bcfv";
+                if(bcfzip.GetEntry(folder + "snapshot.png") != null) {
+                  NewVP.Snapshot = folder + "snapshot.png";
+                  ZipArchiveEntry snap = bcfzip.GetEntry(NewVP.Snapshot);
+                  Bitmap img = new Bitmap(snap.Open());
+                  NewVP.Image = img;
+                } else {
+                  NewVP.Snapshot = "-";
+                  NewVP.Image = null;
+                }
+                readBCFV(bcfzip, folder + NewVP.Bcfv, NewVP);
+                NewTopic.Viewpoints.Add(NewVP);
+              }
+            }
+            // Link Viewpoint and Comments
+            foreach(Comment com in NewTopic.Comments) {
+              if(com.VPGuid != "-") {
+                foreach (Viewpoint vp in NewTopic.Viewpoints) {
+                  if(vp.GUID == com.VPGuid) com.Viewpoint = vp;
+                }
+              }
+            }
+            TopicsList.Add(NewTopic);
+          }
+        }
+      }
+    }
+
+    #endregion
+
+    /// <summary> BCFfile constructor </summary>
+    public BCFfile(string FileName, Boolean Append) {
+      this.ReadBCF(FileName, Append);
+    }
+    /// <summary> BCFfile constructor overload </summary>
+    public BCFfile(string FileName) {
+      this.ReadBCF(FileName, false);
+    }
+    /// <summary> BCFfile constructor overload </summary>
+    public BCFfile() {
+      // Do nothing...
+    }
+  }
+
+}
---
+++ BCFform.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Globalization;
+using BCF_Read;
+
+namespace BCFviewer {
+
+
+  /// <summary> 
+  /// BCFTreeNode if a class derivated from TreeNode with additional properties : <br/>
+  ///   - NodeTopic   : Topic reference if the TReeNodes refers to this type of element <br/>
+  ///   - NodeComment : Comment reference if the TReeNodes refers to this type of element <br/>
+  /// Only one of them shall be defined, the other shall be null
+  /// </summary>
+  public class BCFTreeNode : TreeNode {
+    public Topic NodeTopic;
+    public Comment NodeComment;
+  }
+  
+  /// <Summary> Main class of the BCFform </Summary>
+  public partial class BCFform : Form {
+
+    // Components
+    private SplitContainer Hsplit, Vsplit;
+    private TreeView tree;
+    private ListView list;
+    private PictureBox pict;
+    private Bitmap btmp;
+    // Image ratio
+    private double ratio = 0.8;
+    // BCF file object
+    private BCFfile bcf;
+
+    /// <summary> Add a Topic in the tree </summary>
+    /// <param name="topic">Topic to be added in tree</param>
+    private BCFTreeNode AddTopic(Topic topic) {
+      BCFTreeNode tn = new BCFTreeNode();
+      tn.Text = topic.Title;
+      tn.NodeTopic = topic;
+      tn.NodeComment = null;
+      tree.Nodes.Add(tn);
+      return tn;
+    }
+
+    /// <Summary> Add a Comment in the tree node of the Topic </Summary>
+    /// <param name="comment">Comment to be added in tree node of a Topic</param>
+    private BCFTreeNode AddComment(BCFTreeNode topic, Comment comment) {
+      BCFTreeNode tn = new BCFTreeNode();
+      tn.Text = comment.Text;
+      tn.NodeTopic = null;
+      tn.NodeComment = comment;
+      topic.Nodes.Add(tn);
+      return tn;
+    }
+
+    /// <summary> BCFform constructor </summary>
+    public BCFform()
+    {
+      InitializeComponent();
+      String exe = Application.ExecutablePath;
+      String pat = Path.GetDirectoryName(exe);
+      this.Icon = new Icon( pat + "\\icons\\BCF.ico");
+      this.Padding = new Padding(10, 0, 10, 10);
+      // Menu
+      MenuStrip ms = new MenuStrip();
+      ToolStripMenuItem menu1   = new ToolStripMenuItem("BCF &File");
+      menu1.ShowShortcutKeys = true;
+      ToolStripMenuItem menu2   = new ToolStripMenuItem("&About", null, new EventHandler(About));
+      menu2.ShortcutKeys = Keys.F1;
+      ToolStripMenuItem smenu11 = new ToolStripMenuItem("&Load New BCF file", null, new EventHandler(OpenFile));
+      smenu11.ShortcutKeys = Keys.Control | Keys.O;
+      smenu11.ShortcutKeyDisplayString = "Ctrl-O";
+      smenu11.ShowShortcutKeys = true;
+      ToolStripMenuItem smenu12 = new ToolStripMenuItem("&Append BCF file", null, new EventHandler(AppendFile));
+      smenu12.ShortcutKeys = Keys.Control | Keys.A;
+      ToolStripMenuItem smenu13 = new ToolStripMenuItem("&Quit", null, new EventHandler(Quit));
+      menu1.DropDownItems.Add(smenu11);
+      menu1.DropDownItems.Add(smenu12);
+      menu1.DropDownItems.Add("-");
+      menu1.DropDownItems.Add(smenu13);
+      ((ToolStripDropDownMenu)(menu1.DropDown)).ShowImageMargin = false;
+      ((ToolStripDropDownMenu)(menu1.DropDown)).ShowCheckMargin = true;
+      //ms.MdiWindowListItem = menu1;
+      //ms.MdiWindowListItem = menu2;
+      ms.Items.Add(menu1);
+      ms.Items.Add(menu2);
+      ms.Dock = DockStyle.Top;
+      this.MainMenuStrip = ms;
+      //-----------------------
+      // Tree View
+      tree = new TreeView();
+      tree.TabIndex = 2;
+      tree.Dock = DockStyle.Fill;
+      tree.CheckBoxes = false;
+      tree.AfterSelect += new TreeViewEventHandler(ShowTopicDetails);
+      // List View
+      list = new ListView();
+      list.TabIndex = 3;
+      list.Dock = DockStyle.Fill;
+      list.View = View.Details;
+      list.FullRowSelect = true;
+      list.GridLines = false;
+      // Image
+      pict = new PictureBox();
+      pict.Dock = DockStyle.Fill;
+      pict.TabStop = false;
+      pict.SizeMode = PictureBoxSizeMode.Zoom;
+      pict.BorderStyle = BorderStyle.FixedSingle;
+      pict.BackColor = Color.Beige;
+      // Vertical Split Container
+      Vsplit = new SplitContainer();
+      Vsplit.TabIndex = 1;
+      Vsplit.Dock = DockStyle.Fill;
+      Vsplit.ForeColor = SystemColors.Control;
+      Vsplit.Name = "Vertical Split";
+      Vsplit.Orientation = Orientation.Horizontal;
+      Vsplit.SplitterDistance = 295;
+      Vsplit.SplitterWidth = 10;
+      Vsplit.IsSplitterFixed = true;
+      Vsplit.Panel1.Name = "Image";
+      Vsplit.Panel2.Name = "Properties";
+      Vsplit.Panel2MinSize = 100;
+      // Horizontal Split Container
+      Hsplit = new SplitContainer();
+      Hsplit.TabIndex = 0;
+      Hsplit.Dock = DockStyle.Fill;
+      Hsplit.ForeColor = SystemColors.Control;
+      Hsplit.Name = "Horizontal Split";
+      Hsplit.Orientation = Orientation.Vertical;
+      Hsplit.SplitterDistance = 80;
+      Hsplit.SplitterWidth = 10;
+      Hsplit.SplitterMoved += new SplitterEventHandler(HSplitterMoved);
+      Hsplit.SplitterMoving += new SplitterCancelEventHandler(HSplitterMoving);
+      Hsplit.Panel1.Name = "Topics List";
+      Hsplit.Panel2.Name = "Details";
+      // Fill the form
+      Vsplit.Panel1.Controls.Add(pict);
+      Vsplit.Panel2.Controls.Add(list);
+      Hsplit.Panel1.Controls.Add(tree);
+      Hsplit.Panel2.Controls.Add(Vsplit);
+      this.Controls.Add(Hsplit);
+      this.Controls.Add(ms);
+      // Form
+      this.MinimumSize = new Size(400,400);
+      // Form resize handler
+      this.Resize += new EventHandler(BCFform_Resize);
+      // Resize picture
+      ImageResize();
+      // Construct the BCF file object
+      bcf = new BCFfile();
+    }
+
+    /// <summary> Add a set line with (name, value) in the list view </summary>
+    /// <param name="name">Name of the property</param>
+    /// <param name="value">Value of the property</param>
+    private void AddProperty(string name, string value) {
+      ListViewItem newItem = new ListViewItem(name,0);
+      newItem.SubItems.Add(value);
+      list.Items.Add(newItem);
+    }
+
+    /// <summary> Fills the details of the selected Topic in the list view and the image </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="args">Not used</param>
+    private void ShowTopicDetails(Object sender, TreeViewEventArgs args) {
+      list.Clear();
+      if(this.bcf.TopicsList.Count>0) {
+        BCFTreeNode tn = (BCFTreeNode)tree.SelectedNode;
+        if(tn.NodeComment==null) {
+          list.Columns.Add("TOPIC", -1, HorizontalAlignment.Left);
+          list.Columns.Add("", -1, HorizontalAlignment.Left);
+          Topic topic = tn.NodeTopic;
+          AddProperty("Title", topic.Title);
+          AddProperty("Description",topic.Description);
+          AddProperty("Topic Type",topic.TopicType);
+          AddProperty("Topic Status",topic.TopicStatus);
+          AddProperty("Topic Index",topic.Index.ToString());
+          AddProperty("Creation Date",formatDate(topic.CreationDate));
+          AddProperty("Creation Author",topic.CreationAuthor);
+          AddProperty("Modified Date",formatDate(topic.ModifiedDate));
+          AddProperty("Modified Author",topic.ModifiedAuthor);
+          // Viewpoint
+          if(topic.Viewpoints.Count > 0) {
+            Viewpoint vp = topic.Viewpoints[0];
+            // Viewpoint-Image
+            if(vp.Image != null) {
+              btmp = new Bitmap(vp.Image);
+              pict.Image = btmp;
+              ratio = (double)btmp.Height / (double)btmp.Width;
+              ImageResize();
+            } else { // No Image
+              pict.Image = null;
+            }
+          }
+        } else {
+          list.Columns.Add("COMMENT", -1, HorizontalAlignment.Left);
+          list.Columns.Add("", -1, HorizontalAlignment.Left);
+          Comment comment = tn.NodeComment;
+          AddProperty("Comment", comment.Text);
+          AddProperty("Modified Date",formatDate(comment.ModifiedDate));
+          AddProperty("Modified Author",comment.ModifiedAuthor);
+          BCFTreeNode tp = (BCFTreeNode)tn.Parent;
+          Topic topic = tp.NodeTopic;
+          // Viewpoint
+          Viewpoint vp = null;
+          if(comment.Viewpoint != null) {
+            vp = comment.Viewpoint;
+          } else if(topic.Viewpoints.Count > 0) {
+            vp = topic.Viewpoints[0];
+          }
+          if(vp != null) { // Viewpoint-Image
+            if(vp.Image != null) {
+              btmp = new Bitmap(vp.Image);
+              pict.Image = btmp;
+              ratio = (double)btmp.Height / (double)btmp.Width;
+              ImageResize();
+            } else { // No Image
+              pict.Image = null;
+            }
+          }
+        }
+      }
+      list.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+    }
+
+    /// <summary> Response to the menu event : load BCF file </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="args">Not used</param>
+    private void OpenFile(Object sender, EventArgs args) {
+      LoadFile(false);
+    }
+
+    /// <summary> Response to the menu event : append BCF file </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="args">Not used</param>
+    private void AppendFile(Object sender, EventArgs args) {
+      LoadFile(true);
+    }
+
+    /// <summary> Read or Append a BCF file according to the Append parameter </summary>
+    /// <param name="Append">Load if false or Append if true </param>
+    private void LoadFile(Boolean Append) {
+      using(OpenFileDialog openFileDialog = new OpenFileDialog()) {
+        openFileDialog.Filter = "BCFZIP files (*.bcfzip)|*.bcfzip|BCF files (*.bcf)|*.bcf|All files (*.*)|*.*";
+        openFileDialog.FilterIndex = 1;
+        openFileDialog.RestoreDirectory = true;
+        if(openFileDialog.ShowDialog() == DialogResult.OK) {
+          //Get the path of specified file
+          string filePath = openFileDialog.FileName;
+          // Read the BCF file
+          bcf.ReadBCF(filePath, Append);
+          tree.Nodes.Clear();
+          foreach(Topic topic in bcf.TopicsList) {
+            BCFTreeNode tn = AddTopic(topic);
+            foreach(Comment comment in topic.Comments) {
+              AddComment(tn, comment);
+            }
+          }
+          if(tree.Nodes.Count > 0) tree.SelectedNode = tree.Nodes[0];
+          // Resize picture
+          ImageResize();
+        }
+      }
+    }
+
+    /// <summary> Response to the menu event : About </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="args">Not used</param>
+    private void About(Object sender, EventArgs args) {
+      MessageBox.Show("BCF File Viewer by Emmanuel Maschas - Nov. 2020", "BCF File Viewer");
+    }
+
+    /// <summary> Move the vertical split to match the image ratio (Width / Height) </summary>
+    private void ImageResize() {
+      int maxHeight = Vsplit.Height - Vsplit.Panel2MinSize - Vsplit.SplitterWidth;
+      int newHeight = (int)((double)Vsplit.Width * ratio);
+      if(maxHeight > 0) {
+        Vsplit.SplitterDistance = (newHeight < maxHeight ? newHeight : maxHeight);
+      }
+    }
+
+    /// <summary> Response to the event that occurs when the form is resized </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="args">Not used</param>
+    private void BCFform_Resize(Object sender, EventArgs args) {
+      ImageResize();
+    }
+
+    /// <summary> Response to the event that occurs when the horizontal splitter is moving </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="args">Not used</param>
+    private void HSplitterMoving(Object sender, SplitterCancelEventArgs args) {
+        Cursor.Current = Cursors.VSplit;
+    }
+
+    /// <summary> Response to the event that occurs after the horizontal splitter has been moved </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="args">Not used</param>
+    private void HSplitterMoved(Object sender, SplitterEventArgs args) {
+        Cursor.Current = Cursors.Default;
+        ImageResize();
+    }
+
+    /// <summary> Format an ISO date in readable string </summary>
+    /// <param name="isodate">Date in ISO format. Ex : 2014-10-16T14:35:29+00:00</param>
+    private string formatDate(string isodate) {
+      string res;
+      try { 
+        DateTime dat = DateTime.Parse(isodate);
+        res = dat.ToString("d", CultureInfo.CreateSpecificCulture("fr-FR")); 
+      }
+      catch { res = isodate; }
+      return res;
+    }
+
+    /// <summary> Response to the menu event : Quit </summary>
+    /// <param name="sender">Not used</param>
+    /// <param name="args">Not used</param>
+    private void Quit(Object sender, EventArgs args) {
+      this.Close();
+    }
+  }
+}
